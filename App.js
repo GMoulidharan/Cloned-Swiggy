5c47a3b14d9a80ee1bf1f46d7944a38f30c8586e
GMoulidharan <moulidharansrv@gmail.com>
2024-11-16 11:22:51 +0530

    Episode-2

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-/*
-    <div id="parent">
-        <div id="child">
-            <h1>I'm h1 tag</h1>
-            <h2>I'm h2 tag</h2>
-        </div>
-        <div id="child2">
-            <h1>I'm h1 tag</h1>
-            <h2>I'm h2 tag</h2>
-        </div>
-    </div>
-*/ 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement(
     "div",
     {id:"parent"},
     [React.createElement(
         "div",
         {id:"child"},
-        [React.createElement("h1",{},"I'm an h1 tag"),
+        [React.createElement("h1",{},"I'm an h1 tag react"),
         React.createElement("h2",{},"I'm an h2 tag")]),
     React.createElement(
         "div",
